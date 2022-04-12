89c421512d615c5769d28c69ba6d1f1a25cb245e
Kyle Geske <stungeye@gmail.com>
2022-04-12 12:09:33 -0500

    Commit after implementing the Shape, Rect, and Heart classes and the vector of shapes that a user can add to and remove from.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,91 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::draw() {
+	for (const auto shape : shapes) {
+		shape->render();
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	if (!mouseOnCanvas) { return; }
+
+	int x = ofGetMouseX();
+	int y = ofGetMouseY();
+
+	switch (key) {
+	case 'r': // Add a rectangle. 
+		shapes.push_back(new Rect({x, y}, ofColor::cadetBlue, 100, 200));
+		break;
+	case 'h': // Add a heart.
+		shapes.push_back(new Heart({x, y}, ofColor::darkRed, 100));
+		break;
+	case 'd': // Remove the last added shape. 
+		if (!shapes.empty()) {
+			delete shapes.back(); // Without this line we will leak memory!
+			shapes.pop_back();
+		}
+		break;
+	default:
+		std::cout << "Unknown key: " << static_cast<char>(key) << "\n";
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseEntered(int x, int y) {
+	mouseOnCanvas = true;
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+	mouseOnCanvas = false;
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,24 +1,79 @@
 #pragma once
 
 #include "ofMain.h"
+#include <vector> // std::vector
 
-class ofApp : public ofBaseApp{
+class Shape {
+protected:
+	glm::vec2 position;
+	ofColor color;
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+	Shape(const glm::vec2& position, const ofColor& color)
+		: position{position}, color{color} {
+	}
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	virtual ~Shape() = default;
+	virtual void render() const = 0; // Pure Virtual Function. This will make Shape an Abstract Class.
+};
+
+class Rect : public Shape {
+	int width;
+	int height;
+public:
+	Rect(const glm::vec2& position, const ofColor& color, int width, int height)
+		: Shape{position, color}, width{width}, height{height} {
+	}
+
+	void render() const override {
+		ofSetColor(color);
+		ofDrawRectangle(position.x, position.y, width, height);
+	}
+};
+
+class Heart : public Shape {
+	int size;
+public:
+	Heart(const glm::vec2& position, const ofColor& color, int size)
+		: Shape{position, color}, size{size} {
+	}
+
+	void render() const override {
+		// std::cout << "Drawing Heart of s{" << size << "} @ x: " << position.x << " y: " << position.y << "\n";
+		ofSetColor(color);
+
+		ofBeginShape();
+		float i = 0;
+		while (i < TWO_PI) {
+			float r = (2 - 2 * sin(i) + sin(i) * sqrt(abs(cos(i))) / (sin(i) + 1.4)) * -(size * 0.25);
+			float x = position.x + cos(i) * r;
+			float y = position.y + sin(i) * r;
+			ofVertex(x, y);
+			i += 0.005 * HALF_PI * 0.5;
+		}
+		ofEndShape(true);
+	}
+};
+
+class ofApp : public ofBaseApp {
+
+public:
+	void setup() override;
+	void update() override;
+	void draw() override;
+
+	void keyPressed(int key) override;
+	void keyReleased(int key) override;
+	void mouseMoved(int x, int y) override;
+	void mouseDragged(int x, int y, int button) override;
+	void mousePressed(int x, int y, int button) override;
+	void mouseReleased(int x, int y, int button) override;
+	void mouseEntered(int x, int y) override;
+	void mouseExited(int x, int y) override;
+	void windowResized(int w, int h) override;
+	void dragEvent(ofDragInfo dragInfo) override;
+	void gotMessage(ofMessage msg) override;
+
+	std::vector<Shape*> shapes;
+	bool mouseOnCanvas = false;
 };
